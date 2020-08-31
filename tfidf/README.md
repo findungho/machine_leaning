da79de1b9ed6359b20ba2b834ac2bf7fa16db91f
Dung Ho <fin.dungho@gmail.com>
2020-08-31 16:15:06 +0300

    Machine learning repo

---
+++ tfidf/README.md
@@ -0,0 +1,21 @@
+# Author: Dung Ho
+# Email: dung.ho@edu.turkuamk.fi
+# Phone number: +358 449865555
+
+
+## Overview
+_This is a Python script used to find important word in text using TF-IDF._
+
+
+## General
+- Definition:
+TF-IDF stands for "Term Frequency, Inverse Document Frequency." It's a way to score the importance of words (or "terms") in a document based on how frequently they appear across multiple documents.
+
+- Why?
+If a word appears frequently in a document, it's important. Give the word a high score.
+But if a word appears in many documents, it's not a unique identifier. Give the word a low score.
+Therefore, common words like "the" and "for," which appear in many documents, will be scaled down. Words that appear frequently in a single document will be scaled up.
+
+- This cript uses 'TextBlob' library for processing textual data.
+
+## How to use:
---
+++ tfidf/main.py
@@ -0,0 +1,93 @@
+# Description: This cript is used to compute the TF-IDF of words
+# in a given documents in order to find the importance words.
+# TextBlob is used for breaking up the text into words.
+# Author: Dung Ho
+
+import math
+from textblob import TextBlob as tb
+
+
+def tf(word, doc):
+    """
+    Function computes "term frequency" which is the number of times a word
+    appears in a document blob, normalized by dividing by the total number
+    of words in blob.
+
+    :param word: a given word.
+    :param doc: a document that contains the word to compute.
+
+    :return: a float number.
+    """
+
+    return doc.words.count(word) / len(doc.words)
+
+
+def n_containing(word, docs):
+    """
+    Function calculate the number of documents containing a word.
+
+    :param word: a word.
+    :param docs: a list of documents to check.
+
+    :return an integer number.
+    """
+    list_docs = [1 for doc in docs if word in doc.words]
+
+    return sum(list_docs)
+
+
+def idf(word, docs):
+    """
+    Function computes "inverse document frequency" which measures how common a
+    word is among all documents by taking the ratio of the total number of
+    documents to the number of documents containing word, then take the log
+    of that. Add 1 to the divisor to prevent division by zero.
+    The more common a word is, the lower its idf.
+
+    :param word: a word.
+    :param docs: a list of documents.
+
+    :return: a float number.
+    """
+    return math.log(len(docs) / (1 + n_containing(word, docs)))
+
+
+def tfidf(word, doc, docs):
+    """
+    Function computes the TF-IDF score. Its the product of tf and idf.
+    """
+    tf_idf = tf(word, doc) * idf(word, docs)
+
+    return tf_idf
+
+
+def read_file(f_name):
+    """
+    Function reads a text file, splits into different paragraphs
+    and converts to TextBlob data for processing.
+
+    :param f_name: a text file.
+
+    :return: a list of TextBlob documents.
+    """
+    with open(f_name, "r") as f:
+        ph = f.read().split("\n")
+
+    docs = [tb(i) for i in ph]
+
+    return docs
+
+
+if __name__ == "__main__":
+    docs = read_file("demo/demo.txt")
+
+    for i, doc in enumerate(docs):
+        print("\nParagraph {}:".format(i+1))
+        print(doc)
+        print("\nTop words in paragraph {}:".format(i + 1))
+        frequencies = {word: tfidf(word, doc, docs) for word in doc.words}
+        sorted_words = sorted(
+            frequencies.items(), key=lambda x: x[1], reverse=True
+        )
+        for word, score in sorted_words[:3]:
+            print("\tWord: {}, TF-IDF: {}".format(word, round(score, 5)))
